b8a1c92d7b82a43a6ccd3c5bf1b389c987e29519
Leticia de Castro Jacob Marques <leticiamaca134@gmail.com>
2024-05-03 15:35:36 -0300

    feat: Exercicio 5

---
+++ ex5.js
@@ -0,0 +1,9 @@
+let n1 = parseInt(prompt('Digite um número'))
+let n2 = parseInt(prompt('Digite outro número'))
+let n3 = parseInt(prompt('Digite um outro número'))
+
+if(n3 > n2 & n2 > n1){
+    alert('Está em ordem crescente')
+}else{
+    alert('Não está em ordem crescente')
+}
