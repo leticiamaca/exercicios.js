9d4621d1d0a92c414662c3fdd8f25a673e67610e
Leticia de Castro Jacob Marques <leticiamaca134@gmail.com>
2024-05-03 15:34:23 -0300

    feat: Exercicio 4

---
+++ ex4.js
@@ -0,0 +1,17 @@
+let n1 = parseInt(prompt('Digite a nota do primeiro bimestre: '))
+let n2 = parseInt(prompt('Digite a nota do segundo bimestre: '))
+let n3 = parseInt(prompt('Digite a nota do terceiro bimestre: '))
+let n4 = parseInt(prompt('Digite a nota do quarto bimestre: '))
+
+let total = n1+n2+n3+n4
+let media= total/4
+
+if(media<5){
+    alert('Infelizmente você foi Reprovado')
+}
+if(media >5 || media <7){
+alert('Recuperação')
+}
+if(media>=7){
+    alert('Aprovado!')
+}
