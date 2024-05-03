546b3f059709c5f2514f5183f6679302d4409c88
Leticia de Castro Jacob Marques <leticiamaca134@gmail.com>
2024-05-03 15:19:04 -0300

    feat: exercicio 2

---
+++ ex2.js
@@ -0,0 +1,25 @@
+var numero = prompt('Digite um número')
+
+switch (numero){
+    case '1':
+        alert('segunda-feira');
+        break;
+        case '2': 
+        alert('terça-feira')
+        break;
+        case '3':
+            alert('Quarta-feira')
+            break;
+            case '4':
+                alert('Quinta-feira')
+                break
+            case '5':
+                alert('Sexta-feira')
+                break
+            case '6':
+                alert('Sábado')
+                break
+            case '7':
+                alert('domingo')
+                break
+}
