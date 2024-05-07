1209db8c75372f588037db8ed2964d55edfb3333
Leticia de Castro Jacob Marques <leticiamaca134@gmail.com>
2024-05-07 17:19:34 -0300

    feat:Exercicio 4 - For

---
+++ loop/ex4.js
@@ -0,0 +1,10 @@
+// Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+
+
+let num = parseInt(prompt('Digite um número'));
+
+for (contador = 1; contador <= 10; contador++){
+    console.log(num*contador)
+}
+  
